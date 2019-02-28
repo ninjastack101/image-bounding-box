0cb0ceee9725fe278ee84f134ef825a94857f016
ninjastack101 <ninjastack101@gmail.com>
2019-02-28 20:36:45 +0530

    Models for app

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from jsonfield import JSONField
+
+
 
 # Create your models here.
+class Task(models.Model):
+    image_detail = models.TextField(max_length=1000, null=True, blank=True)
+    name = models.TextField(max_length=1000, null=True, blank=True)
+    box = JSONField(null=True, blank=True)
+    polyLines = JSONField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.image_detail
+        
