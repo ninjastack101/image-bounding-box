188ee386c5d70e47f8bb4b09e74f859dad23ebb1
ninjastack101 <ninjastack101@gmail.com>
2019-02-28 20:42:12 +0530

    Admin pannel file

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from myapp.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('image_detail',)
+
+
+admin.site.register(Task, TaskAdmin)
